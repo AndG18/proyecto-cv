efd03d5149758147c4228a6e6f206e0a8d94ec12
Monarca <77404732+AndG18@users.noreply.github.com>
2023-05-25 16:03:21 -0500

    Add files via upload

---
+++ proyecto_pagina_hoja_de_vida ver 1.3/d.js
@@ -0,0 +1,31 @@
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  window.addEventListener('scroll', function() {
+    var button = document.getElementById('scrollTopButton');
+    if (window.scrollY > 500) {
+      button.style.display = 'block';
+    } else {
+      button.style.display = 'none';
+    }
+  });
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  window.addEventListener('scroll', function() {
+    var button = document.getElementById('scrollTopButton');
+    if (window.scrollY > 500) {
+      button.style.display = 'block';
+    } else {
+      button.style.display = 'none';
+    }
+  });
+    
